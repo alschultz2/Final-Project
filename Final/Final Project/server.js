5352230445c4c9eeaca52c27656ad8309f190223
alschultz2 <89539149+alschultz2@users.noreply.github.com>
2023-05-11 18:54:17 -0500

    Update server.js

--- Final/Final Project/server.js
+++ Final/Final Project/server.js
@@ -19,16 +19,7 @@ app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'))
 app.use('/states', require('./routes/states'))
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 file is not found" });
-    } else {
-        res.type('txt').send("404 file is not found");
-    }
-});
+app.all('*', (req, res) => {});
 mongoose.connection.once('open', () => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
